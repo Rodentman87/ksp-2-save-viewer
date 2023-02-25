c95c8cc4fa7705c6fec925d9528a40ee5dc6c455
Maisy <rodentman87@gmail.com>
2023-02-25 14:49:21 -0600

    Add last save file storage for quick loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { EuiIcon, EuiProvider, EuiToolTip } from "@elastic/eui";
+import { EuiButton, EuiIcon, EuiProvider, EuiToolTip } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_dark.css";
 // import "@elastic/eui/dist/eui_theme_light.css";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 import { createHashRouter, RouterProvider } from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
 import { Layout } from "./components/Layout";
 import { Agencies } from "./pages/Agencies";
 import { Home } from "./pages/Home";
@@ -13,6 +14,7 @@ import { loadSaveData } from "./save-helper/loadSaveData";
 import { Save } from "./save-helper/Save";
 import { SaveFileContext } from "./SaveFileContext";
 import { ThemeContext } from "./ThemeContext";
+import { SaveFile_0_1_0 } from "./types/save/SaveFile-0-1-0";
 
 const router = createHashRouter([
 	{
@@ -55,6 +57,10 @@ function App() {
 	const [saveFile, setSaveFile] = React.useState<Save | null>(null);
 	const [error, setError] = React.useState<string | null>(null);
 	const [theme, setTheme] = useState(false);
+	const [lastSaveFile, setLastSaveFile] =
+		useLocalStorageState<SaveFile_0_1_0 | null>("lastSaveFile", {
+			defaultValue: null,
+		});
 
 	const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
 		setError(null);
@@ -69,6 +75,7 @@ function App() {
 			return;
 		}
 		const body = await file.text();
+		setLastSaveFile(JSON.parse(body));
 		setSaveFile(loadSaveData(body));
 	};
 
@@ -84,36 +91,76 @@ function App() {
 		>
 			<EuiProvider colorMode={theme ? "light" : "dark"}>
 				<div className="w-screen h-screen flex flex-col bg-slate-800 pt-12">
-					<AnimatePresence mode="wait">
-						{saveFile !== null ? (
-							<SaveFileContext.Provider value={saveFile}>
-								<RouterProvider router={router} />
-							</SaveFileContext.Provider>
-						) : (
-							<div className="p-6 rounded-md bg-white shadow-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-								<h1 className="text-2xl font-bold mb-2 text-black">
-									Please Upload a Save File (.json)
-									<EuiToolTip
-										position="right"
-										content="Saves can be found in 'users/[username]/AppData/LocalLow/Intercept Games/Kerbal Space Program 2/Saves/SinglePlayer/[savename]/'"
+					{saveFile !== null ? (
+						<SaveFileContext.Provider value={saveFile}>
+							<RouterProvider router={router} />
+						</SaveFileContext.Provider>
+					) : (
+						<div className="flex flex-col gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+							<AnimatePresence>
+								<motion.div
+									key="upload"
+									layoutId="upload"
+									className="p-6 rounded-md bg-white shadow-lg"
+								>
+									<h1 className="text-2xl font-bold mb-2 text-black">
+										Please Upload a Save File (.json)
+										<EuiToolTip
+											position="right"
+											content="Saves can be found in 'users/[username]/AppData/LocalLow/Intercept Games/Kerbal Space Program 2/Saves/SinglePlayer/[savename]/'"
+										>
+											<EuiIcon
+												tabIndex={0}
+												type="questionInCircle"
+												className="ml-1 mb-1"
+											/>
+										</EuiToolTip>
+									</h1>
+									<input
+										type="file"
+										className="text-black"
+										multiple
+										onChange={handleFileChange}
+									/>
+									{error && <p className="text-red-500 mt-2">{error}</p>}
+								</motion.div>
+								{lastSaveFile && (
+									<motion.div
+										key="lastSaveFile"
+										layoutId="lastSaveFile"
+										initial={{ y: 10, opacity: 0 }}
+										animate={{ y: 0, opacity: 1 }}
+										exit={{ y: 10, opacity: 0 }}
+										className="p-6 rounded-md bg-white shadow-lg"
 									>
-										<EuiIcon
-											tabIndex={0}
-											type="questionInCircle"
-											className="ml-1 mb-1"
-										/>
-									</EuiToolTip>
-								</h1>
-								<input
-									type="file"
-									className="text-black"
-									multiple
-									onChange={handleFileChange}
-								/>
-								{error && <p className="text-red-500 mt-2">{error}</p>}
-							</div>
-						)}
-					</AnimatePresence>
+										<h2 className="text-xl font-bold mb-2 text-black">
+											Last used save file: {lastSaveFile.Metadata.Name}
+										</h2>
+										<div className="flex flex-row justify-end gap-2">
+											<EuiButton
+												color="danger"
+												onClick={() => {
+													setLastSaveFile(null);
+												}}
+											>
+												Clear
+											</EuiButton>
+											<EuiButton
+												color="primary"
+												onClick={() => {
+													setSaveFile(
+														loadSaveData(JSON.stringify(lastSaveFile))
+													);
+												}}
+											>
+												Load
+											</EuiButton>
+										</div>
+									</motion.div>
+								)}
+							</AnimatePresence>
+						</div>
+					)}
 				</div>
 			</EuiProvider>
 		</ThemeContext.Provider>
